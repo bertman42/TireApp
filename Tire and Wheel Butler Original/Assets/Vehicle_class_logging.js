7240fd6f3f4a7ab6f0baff2d2183801c51f75125
Bert Carapiet <Bert Carapiet>
2016-04-23 09:37:54 -0400

    Adding app to Repo

---
+++ Tire and Wheel Butler Original/Assets/Vehicle_class_logging.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+    Debug.Log("I am alive!");
+}
+
+function Update () {
+
+}
---
+++ Tire and Wheel Butler Original/Assets/Webcam_devices.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Webcam_devices : MonoBehaviour {
+	// Gets the list of devices and prints them to the console.
+	public void Start() {
+		WebCamDevice[] devices = WebCamTexture.devices;
+		for( var i = 0 ; i < devices.Length ; i++ )
+			Debug.Log(devices[i].name);        
+	}
+}
